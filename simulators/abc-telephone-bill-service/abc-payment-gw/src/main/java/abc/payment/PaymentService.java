c23e24eb178f1b56577469e0326532e2911191e7
Chandana <chandana@chandana-desktop.(none)>
2012-12-11 21:12:24 +0530

    Added WSS4J Spring configurations to abc telephone simulator

--- simulators/abc-telephone-bill-service/abc-payment-gw/src/main/java/abc/payment/PaymentService.java
+++ simulators/abc-telephone-bill-service/abc-payment-gw/src/main/java/abc/payment/PaymentService.java
@@ -1,11 +1,15 @@
 package abc.payment;
 
 import javax.jws.WebMethod;
+import javax.jws.WebParam;
 import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
 
 @WebService(targetNamespace = "http://localhost:8080/abc-payment-gw")
+@SOAPBinding
 public interface PaymentService {
 
+    @WebMethod
     PaymentResponse payBill(PaymentRequest paymentRequest);
     
 }
