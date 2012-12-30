3d4b027e7fdd82ab755ccf2f61eb01f5e760c400
Melan Nimesh <melan@wso2.com>
2012-12-30 10:37:31 +0530

    Added missing class 'Account'

---
+++ bank-system-core/portal-core/src/main/java/org/team5/portal/data/Account.java
@@ -0,0 +1,33 @@
+package org.team5.portal.data;
+
+public class Account {
+
+	private String accountNo;
+	private String accountName;
+	private String userId;
+
+	public String getAccountNo() {
+		return accountNo;
+	}
+
+	public void setAccountNo(String accountNo) {
+		this.accountNo = accountNo;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public void setAccountName(String accountName) {
+		this.accountName = accountName;
+	}
+
+	public String getAccountName() {
+		return accountName;
+	}
+
+}
